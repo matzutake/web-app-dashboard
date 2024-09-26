b92536e85e374a084011bf3b76a00d06fd55b47d
pahmutov <pahmutov@f5.com.ru>
2024-09-26 13:09:38 +0300

    created transactions

--- components/DataTable.vue
+++ components/DataTable.vue
@@ -1,33 +1,72 @@
-<script setup lang="ts">
+<script setup lang="ts" generic="TData, TValue">
+import type { ColumnDef } from "@tanstack/vue-table";
+import { FlexRender, getCoreRowModel, useVueTable } from "@tanstack/vue-table";
+
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+
+const props = defineProps<{
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}>();
+
+const table = useVueTable({
+  get data() {
+    return props.data;
+  },
+  get columns() {
+    return props.columns;
+  },
+  getCoreRowModel: getCoreRowModel(),
+});
 </script>
 
 <template>
-  <Table>
-    <TableCaption>A list of your recent invoices.</TableCaption>
-    <TableHeader>
-      <TableRow>
-        <TableHead class="w-[100px]"> Invoice </TableHead>
-        <TableHead>Status</TableHead>
-        <TableHead>Method</TableHead>
-        <TableHead class="text-right"> Amount </TableHead>
-      </TableRow>
-    </TableHeader>
-    <TableBody>
-      <TableRow>
-        <TableCell class="font-medium"> INV001 </TableCell>
-        <TableCell>Paid</TableCell>
-        <TableCell>Credit Card</TableCell>
-        <TableCell class="text-right"> $250.00 </TableCell>
-      </TableRow>
-    </TableBody>
-  </Table>
+  <div class="border rounded-md">
+    <Table>
+      <TableHeader>
+        <TableRow
+          v-for="headerGroup in table.getHeaderGroups()"
+          :key="headerGroup.id"
+        >
+          <TableHead v-for="header in headerGroup.headers" :key="header.id">
+            <FlexRender
+              v-if="!header.isPlaceholder"
+              :render="header.column.columnDef.header"
+              :props="header.getContext()"
+            />
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <template v-if="table.getRowModel().rows?.length">
+          <TableRow
+            v-for="row in table.getRowModel().rows"
+            :key="row.id"
+            :data-state="row.getIsSelected() ? 'selected' : undefined"
+          >
+            <TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
+              <FlexRender
+                :render="cell.column.columnDef.cell"
+                :props="cell.getContext()"
+              />
+            </TableCell>
+          </TableRow>
+        </template>
+        <template v-else>
+          <TableRow>
+            <TableCell :colspan="columns.length" class="h-24 text-center">
+              No results.
+            </TableCell>
+          </TableRow>
+        </template>
+      </TableBody>
+    </Table>
+  </div>
 </template>
--- pages/transactions/index.vue
+++ pages/transactions/index.vue
@@ -6,12 +6,40 @@
         <h1>Transactions</h1>
       </div>
     </header>
-    <DataTable />
+    <DataTable :data="data" :columns="columns" />
   </div>
 </template>
 
-<script setup>
-const loading = ref(false);
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import { columns } from "./columns";
 
-const data = ref < any > [];
+const data = ref<any>([]);
+
+function generateRandomData(numObjects: any) {
+  const statuses = ["pending", "processing", "completed"];
+  const emails = [
+    "example@gmail.com",
+    "m@example.com",
+    "text@example.com",
+    "user@example.com",
+  ];
+
+  const randomData: any[] = [];
+
+  for (let i = 0; i < numObjects; i++) {
+    const id = Math.random().toString(36).substring(2, 8);
+    const amount = Math.floor(Math.random() * 500) + 50;
+    const status = statuses[Math.floor(Math.random() * statuses.length)];
+    const email = emails[Math.floor(Math.random() * emails.length)];
+
+    randomData.push({ id, amount, status, email });
+  }
+
+  return (data.value = randomData);
+}
+
+onMounted(() => {
+  generateRandomData(10);
+});
 </script>
